a77cb8e5fea9514fdf69a8d7989028bc553569ac
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2024-04-28 21:02:21 +0530

    styling and implementing toggle menu and login and sigup
    @Gklf5 @abhinavjoyc @diyabenny  pls check

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { logout } from "../../redux/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../redux/userSlice"; 
 import { Avatar } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import ChatIcon from "@mui/icons-material/Chat";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import FolderIcon from "@mui/icons-material/Folder"; // New icon for projects
-import AccountCircleIcon from "@mui/icons-material/AccountCircle"; // New icon
-import LockIcon from "@mui/icons-material/Lock"; // Icon for login
-import "./Menu.css"; // Import CSS styles
+import FolderIcon from "@mui/icons-material/Folder";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LockIcon from "@mui/icons-material/Lock";
+import InfoIcon from "@mui/icons-material/Info";
+import MenuIcon from "@mui/icons-material/Menu";
+import "./Menu.css";
 
 const Menu = () => {
-  const currentUser = useSelector((state) => state.user.currentUser);
+  const [isMinimized, setIsMinimized] = useState(true);  
+  const currentUser = useSelector(state => state.user.currentUser);
   const dispatch = useDispatch();
-  const handleLogout = (e) => {
-    e.preventDefault();
+
+  const handleLogout = () => {
     dispatch(logout());
-    console.log("log out success");
+  };
+
+  const toggleSidebar = () => {
+    setIsMinimized(!isMinimized);
   };
 
   return (
-    <div className="sidenav">
+    <div className={`sidenav ${isMinimized ? "minimized" : ""}`}>
+      <div className="toggle-button" onClick={toggleSidebar}>
+        <MenuIcon />
+      </div>
       <div className="sidenav__buttons">
-        <div className="app-name">SocialFlow</div> {/* Added app name here */}
+        <div className="app-name">{isMinimized ? "SF" : "SocialFlow"}</div>
         <Link to="/" className="sidenav__button">
           <HomeIcon />
           <span>Home</span>
@@ -35,49 +43,43 @@ const Menu = () => {
           <SearchIcon />
           <span>Search</span>
         </Link>
-        <Link to="/messages" className="sidenav__button">
-          <ChatIcon />
-          <span>Messages</span>
-        </Link>
-        <Link to="/notifications" className="sidenav__button">
-          <NotificationsIcon />
-          <span>Notifications</span>
-        </Link>
-        <Link to="/projects" className="sidenav__button">
-          {" "}
-          {/* New link for projects */}
-          <FolderIcon /> {/* Icon for projects */}
-          <span>Projects</span>
-        </Link>
-        <Link to="/Addproject" className="sidenav__button">
-          <AddCircleOutlineIcon />
-          <span>Create Project</span>
-        </Link>
         {currentUser ? (
-          <div className="sidenav__button">
-            <Avatar>{currentUser.name.charAt(0).toUpperCase()}</Avatar>
-            <span>{currentUser.name}</span>
-          </div>
+          <>
+            <Link to="/messages" className="sidenav__button">
+              <ChatIcon />
+              <span>Messages</span>
+            </Link>
+            <Link to="/notifications" className="sidenav__button">
+              <NotificationsIcon />
+              <span>Notifications</span>
+            </Link>
+            <Link to="/projects" className="sidenav__button">
+              <FolderIcon />
+              <span>Projects</span>
+            </Link>
+            <Link to="/addproject" className="sidenav__button">
+              <AddCircleOutlineIcon />
+              <span>Create Project</span>
+            </Link>
+            <Link to="/profile" className="sidenav__button">
+              <AccountCircleIcon />
+              <span>Profile</span>
+            </Link>
+            <div className="sidenav__button logout__button" onClick={handleLogout}>
+              <span>Logout</span>
+            </div>
+          </>
         ) : (
-          <Link to="/login" className="sidenav__button">
-            {" "}
-            {/* Link to the login page */}
-            <LockIcon />
-            <span>Login</span>
-          </Link>
-        )}
-        <Link to="/profile" className="sidenav__button">
-          {" "}
-          {/* Link to the profile page */}
-          <AccountCircleIcon />
-          <span>Profile</span>
-        </Link>
-        {currentUser ? (
-          <div className="sidenav__button">
-            <span onClick={handleLogout}>LogOut</span>
-          </div>
-        ) : (
-          <></>
+          <>
+            <Link to="/about" className="sidenav__button">
+              <InfoIcon />
+              <span>About</span>
+            </Link>
+            <Link to="/login" className="sidenav__button">
+              <LockIcon />
+              <span>Login</span>
+            </Link>
+          </>
         )}
       </div>
     </div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,12 +3,15 @@ import axios from "axios";
 import "./Login.css";
 import { useDispatch } from "react-redux";
 import { loginFailure, loginStart, loginSucces } from "../../redux/userSlice";
+
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("creator");
   const dispatch = useDispatch();
+
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
@@ -21,6 +24,7 @@ const Login = () => {
       console.log(res.data);
     } catch (err) {}
   };
+
   const handleLogin = async (e) => {
     e.preventDefault();
     dispatch(loginStart());
@@ -32,51 +36,32 @@ const Login = () => {
       dispatch(loginFailure());
     }
   };
+
   return (
     <div className="login-main">
-      <div className="login-content">
-        <h1 className="title">Sign in</h1>
-        <input
-          placeholder="username"
-          className="input-field"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="input-field"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="sign-btn" onClick={handleLogin}>
-          Signin
-        </button>
-        <h1 className="title">Sign up</h1>
-        <input
-          placeholder="username"
-          className="input-field"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          placeholder="email"
-          className="input-field"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="input-field"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <select
-          className="input-field"
-          onChange={(e) => setRole(e.target.value)}
-        >
-          <option value="creator">Creator</option>
-          <option value="editor">Editor</option>
-        </select>
-        <button className="sign-btn" onClick={handleSignup}>
-          Signup
-        </button>
+      <div className="form-container">
+        {isLogin ? (
+          <div className="login-form">
+            <h1 className="title">Sign in</h1>
+            <input placeholder="username" className="input-field" onChange={(e) => setName(e.target.value)} />
+            <input type="password" placeholder="password" className="input-field" onChange={(e) => setPassword(e.target.value)} />
+            <button className="sign-btn" onClick={handleLogin}>Sign in</button>
+            <p className="toggle" onClick={() => setIsLogin(false)}>Need an account? Sign up</p>
+          </div>
+        ) : (
+          <div className="signup-form">
+            <h1 className="title">Sign up</h1>
+            <input placeholder="username" className="input-field" onChange={(e) => setName(e.target.value)} />
+            <input placeholder="email" className="input-field" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="password" className="input-field" onChange={(e) => setPassword(e.target.value)} />
+            <select className="input-field" onChange={(e) => setRole(e.target.value)}>
+              <option value="creator">Creator</option>
+              <option value="editor">Editor</option>
+            </select>
+            <button className="sign-btn" onClick={handleSignup}>Sign up</button>
+            <p className="toggle" onClick={() => setIsLogin(true)}>Already have an account? Sign in</p>
+          </div>
+        )}
       </div>
     </div>
   );
