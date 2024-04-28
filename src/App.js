2fd2230af383174be504717e4f0533785f118116
Gklf5 <adithya.eg5@gmail.com>
2024-04-28 19:47:41 +0530

    test

--- src/App.js
+++ src/App.js
@@ -10,26 +10,32 @@ import Projects from "./pages/projects/Projects";
 import AddProject from "./pages/Addproject/AddProject";
 import Users from "./pages/Users/Users.jsx";
 import UploadPage from "./pages/upload/upload.jsx";
+import AddSocialForm from "./pages/profile/Add_socials.jsx";
+import UserPage from "./pages/userPage/UserPage.jsx";
 const App = () => {
   return (
     <div className="main">
-      <BrowserRouter>
-        <Menu />
-        <div className="content">
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/post" element={<Post />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/upload" element={<UploadPage />} />
-              <Route path="/addproject" element={<AddProject />} />
-              <Route path="/users" element={<Users />} />
-            </Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="container">
+        <BrowserRouter>
+          <Menu />
+          <div className="content">
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/post" element={<Post />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/upload" element={<UploadPage />} />
+                <Route path="/addproject" element={<AddProject />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/test/addsocials" element={<AddSocialForm />} />
+                <Route path="/test/user" element={<UserPage />} />
+              </Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
---
+++ src/pages/profile/Add_socials.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import "./Profile.css";
+const AddSocialForm = () => {
+  const handleSubmit = (e) => {};
+  const [media, setMedia] = useState("");
+  const [apiKey, setApiKey] = useState("");
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="main-form">
+        <label htmlFor="media">Select Media:</label>
+        <select
+          id="media"
+          value={media}
+          onChange={(e) => setMedia(e.target.value)}
+          required
+        >
+          <option value="">Select Media</option>
+          <option value="youtube">YouTube</option>
+          <option value="twitter">Twitter</option>
+          {/* Add more options as needed */}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="apiField">API Field:</label>
+        <input
+          type="text"
+          id="apiField"
+          value={apiKey}
+          onChange={(e) => setApiKey(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Continue</button>
+    </form>
+  );
+};
+
+export default AddSocialForm;
